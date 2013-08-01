5b77b253465039affe7ca1bfd85b0684d3600329
Zafer Sawaf <zafer.sawaf@gmail.com>
2013-07-31 20:29:48 -0400

    Second commit payment form

--- js/tajer-js.js
+++ js/tajer-js.js
@@ -1,12 +1,70 @@
 
 $(document).ready(function() {
+	
+	// Clear alert box.
+	$(".alert").css("display", "none");
+	
 	// retrieve session token.
 	var token = $.cookie("tajer_token");
 	
 	// initialize variables to store information.
 	var payments;
 	
+	/*
+	* ------------------------------------------------------------
+	* ------------- CREATE PAYMENTS ------------------------------
+	* ------------------------------------------------------------
+	*/
+	$("#paymentSubmit").click(function(){
+		// first part is error checking.
+		var amount = $("#amount").val();
+		var cur = $("#currency").val();
+		var card = $("#card").val();
+
+		// error check amount
+		// should not be negative
+		if (amount < 0) {
+			$(".alert").css("display", "block");
+			$(".alert").html("Amount should be a positive value");
+		}
+		else if (!amount){
+			$(".alert").css("display", "block");
+			$(".alert").html("Please fill in amount");
+		}
+		else {
+			// reset error
+			$(".alert").css("display", "none");
+		}
+		// check credit card
+		/* IMPLEMENT THIS LATER */
+
+		// send data to server.
+
+		var payment = {"amount": amount, "currency": cur, "card":card};
+		$.ajax({
+			url: "http://api.lvh.me:3000/v1/payments",
+			data: payment,
+			type: "POST",
+			xhrFields: {
+				withCredentials: true
+				},
+		    beforeSend: function(xhr) {
+		    	xhr.setRequestHeader("Authorization", "Basic " + token);
+		  	},
+			success: function(response){
+		        console.log(response);
+				alert("payment successful");
+			},
+			error: function(message){
+				$(".alert").css("display", "block");
+				var parsedResponse = $.parseJSON(message.responseText);
+				$(".alert").html(parsedResponse.error.message);
+			}
+		});
+	});
+	
 	// Get basic user information from server.
+	/*
 	$.ajax({
 		url: "http://api.lvh.me:3000/v1/accounts/login",
 		data: cred,
@@ -25,6 +83,7 @@ $(document).ready(function() {
 			alert("retrieving customers failed.");
 		}
 	});
+	*/
 });
 
 /* Logout user */
@@ -38,4 +97,5 @@ function logout(){
 		window.location.replace("login.html");	
 	});
 }
+
 
