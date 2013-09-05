e4e42871982316c216eefa8a331c9fc21a03d05c
Zafer Sawaf <zafer.sawaf@gmail.com>
2013-09-04 23:43:51 -0400

    First draft, new payment layout

--- js/tajer-js.js
+++ js/tajer-js.js
@@ -32,16 +32,7 @@ $(document).ready(function() {
 	* ------------------------------------------------------------
 	*/
 	
-	// Initialize skeuocard.
-	card = new Skeuocard($("#skeuocard"), {
-	    initialValues: {
-	    number: "4111111111111111",
-	    expMonth: "1",
-	    expYear: "2016"
-	    name: "James Doe",
-	    cvc: "123"
-	  }
-	});
+
   
 	$("#paymentSubmit").click(function(){
 				
